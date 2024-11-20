2b699c80599e09891e02c917b9811ae450636090
cirosilvano-mov <ciro.silvano@moviri.com>
2024-11-20 17:39:01 +0100

    support custom ports

--- content/observer.js
+++ content/observer.js
@@ -1,8 +1,11 @@
 const initializeObserver = () => {
     const observer = new MutationObserver(() => {
         if (window.location.href.includes("/saved/searches")) {
-            const domain = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/${window.location.pathname.split('/')[1]}`;
-            const user = extractSplunkUser();
+            const port = window.location.port || 
+                (window.location.protocol === "https:" ? "443" : 
+                (window.location.protocol === "http:" ? "80" : ""));
+            const domain = `${window.location.protocol}//${window.location.hostname}:${port}/${window.location.pathname.split('/')[1]}`;
+			const user = extractSplunkUser();
             enhancePage(domain, user);
         }
     });
